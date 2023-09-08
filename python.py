41eeddf79dbe5d9e59c0619c6edd5fd40d2f44e7
sylla-seiku <seikusylla@gmail.com>
2023-09-08 11:16:22 -0400

    Weight Converter

--- python.py
+++ python.py
@@ -1,3 +1,16 @@
+# Learing Weight Converter
+weight = int(input('Weight: '))
+unit = input('(L)bs or (K)g:')
+if unit.upper() == "L":
+    converted = weight * 0.45
+    print(f"You are {converted} kilos")
+else:
+    converted = weight / 0.45
+    print(f"You are {converted} pounds")
+
+
+
+'''
 #learning about Comparison Operators
 temperature = 35
 
@@ -18,7 +31,7 @@ else:
 
 
 
-'''
+
 # Learning About Logical Operators
 has_high_income = False
 has_good_credit = True
